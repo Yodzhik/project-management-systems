e04033e561f9cde690abe68dd13cdbdfbb6a1184
Alim Chechenov <chechenov321@gmail.com>
2025-04-20 14:08:58 +0300

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css'
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import Issues from "./pages/Issues.jsx";
 import Boards from "./pages/Boards.jsx";
 import Board from "./pages/Board.jsx";
@@ -13,6 +13,7 @@ function App() {
             <Route path='/issues' element={<Issues/>}/>
             <Route path='/boards' element={<Boards/>}/>
             <Route path='/board/:id' element={<Board/>}/>
+            <Route path='*' element={<Navigate to="/issues" replace />} />
         </Routes>
     )
 }
